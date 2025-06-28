d17466b2b2c71bd1bc47bfd7e1c8f339cfb4f710
hyojoon Lim <hyojoon42@naver.com>
2025-06-28 16:53:02 +0900

    feature : 9강. CrossHair 구현

---
+++ Assets/Scripts/CrossHair.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class CrossHair : MonoBehaviour
+{
+    [Header("연결 컴포넌트")]
+    public Animator animator;
+    public GameObject go_CrossHairHUD;          //크로스헤어 활성화/비활성화
+    public GunController gunController;
+
+    private float gunAccuracy;      //정확도
+
+    //------------------- 크로스헤어 애니메이션 재생 -----------------------
+    public void WalkingAnimation(bool _flag)
+    {
+        animator.SetBool("Walking", _flag);
+    }
+    public void RunningAnimation(bool _flag)
+    {
+        animator.SetBool("Running", _flag);
+    }
+    public void CrouchingAnimation(bool _flag)
+    {
+        animator.SetBool("Crouching", _flag);
+    }
+    public void FineSightAnimation(bool _flag)
+    {
+        animator.SetBool("FineSight", _flag);
+    }
+    public void FireAnimation()
+    {
+        if (animator.GetBool("Walking"))
+            animator.SetTrigger("Walk_Fire");
+        else if (animator.GetBool("Crouching"))
+            animator.SetTrigger("Crouch_Fire");
+        else
+            animator.SetTrigger("Idle_Fire");
+    }
+
+    //--------------------- 반동 수치 설정 ----------------------
+    public float GetAccuracy()
+    {
+        if (animator.GetBool("Walking"))
+            gunAccuracy = 0.06f;
+        else if (animator.GetBool("Crouching"))
+            gunAccuracy = 0.01f;
+        else if (gunController.GetFineSightMode())
+            gunAccuracy = 0.001f;
+        else
+            gunAccuracy = 0.035f;
+
+        return gunAccuracy;
+    }
+}
