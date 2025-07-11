fc8c6d07a085a374f29a2cca4a226389e6c6c5d1
hyojoon Lim <hyojoon42@naver.com>
2025-07-11 20:56:51 +0900

    feature : 33강. Light 구현

---
+++ Assets/Scripts/DayAndNight.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class DayAndNight : MonoBehaviour
+{
+    [Header("해 속성")]
+    [SerializeField]
+    private float secondPerRealTimeSecond;              //게임 100초 = 현실 1초
+    [SerializeField]
+    private float nightFogDensity;                      //밤 상태의 Fog 밀도
+    [SerializeField]
+    private float fogDensityCalc;                       //증감량 비율
+
+
+    private float dayFogDensity;                        //낮 상태의 Fog 밀도
+    private float currentFogDensity;                    //계산
+
+    private bool isNight;           //밤인지 아닌지
+
+
+    void Start()
+    {
+        dayFogDensity = RenderSettings.fogDensity;
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.right, 0.1f * secondPerRealTimeSecond * Time.deltaTime);
+
+        if (transform.eulerAngles.x >= 170)
+            isNight = true;
+        else if (transform.eulerAngles.x >= 340)
+            isNight = false;
+
+        if(isNight)
+        {
+            if(currentFogDensity <= nightFogDensity)
+            {
+                currentFogDensity += 0.1f * fogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = currentFogDensity;
+            }
+        }
+        else
+        {
+            if(currentFogDensity >= dayFogDensity)
+            {
+                currentFogDensity -= 0.1f * fogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = currentFogDensity;
+            }
+        }
+    }
+}
