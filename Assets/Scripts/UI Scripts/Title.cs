9b3469a59669f03346a25ac3bb12f997249ef946
hyojoon Lim <hyojoon42@naver.com>
2025-07-12 23:19:46 +0900

    feature : 37강. Title 기능 구현

---
+++ Assets/Scripts/UI Scripts/Title.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public string sceneName = "GameStage";        //씬 이름
+
+
+    //----------------------------- 씬 체인지 --------------------------------
+    public void ClickStart()
+    {
+        Debug.Log("로딩");
+        SceneManager.LoadScene(sceneName);
+    }
+    public void ClickLoad()
+    {
+        Debug.Log("로드");
+    }
+    public void ClickExit()
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
+    }
+
+}
